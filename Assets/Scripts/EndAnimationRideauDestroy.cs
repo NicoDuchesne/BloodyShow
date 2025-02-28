5ee435963335b06fb62900dbf71c6f6470e140ee
Rayan <rayanngi20@gmail.com>
2025-02-28 15:05:15 +0100

    add animation rideau

---
+++ Assets/Scripts/EndAnimationRideauDestroy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAnimationRideauDestroy : MonoBehaviour
+{
+    public GameObject rideau;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rideau.SetActive(true);
+    }
+
+    public void RideauOff()
+    {
+        rideau.SetActive(false);
+    }
+
+}
