c5b7b7e5c085fb90ed28d913df5465496c31ddc7
Rayan <rayanngi20@gmail.com>
2025-03-03 19:26:05 +0100

    test

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/PlayGameManager.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/PlayGameManager.cs
@@ -4,32 +4,39 @@ using UnityEngine;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 
-
 public class PlayGameManager : MonoBehaviour
 {
-
     void Start()
     {
+        Debug.Log("Tentative de connexion...");
         SignIn();
     }
 
     public void SignIn()
     {
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+        Debug.Log("Authentification en cours...");
     }
 
     internal void ProcessAuthentication(SignInStatus status)
     {
+        Debug.Log("Statut de connexion : " + status);
+
         if (status == SignInStatus.Success)
         {
             string name = PlayGamesPlatform.Instance.GetUserDisplayName();
             string id = PlayGamesPlatform.Instance.GetUserId();
             string ImmgUrl = PlayGamesPlatform.Instance.GetUserImageUrl();
+
+            Debug.Log("Connexion réussie !");
+            Debug.Log("Nom: " + name);
+            Debug.Log("ID: " + id);
+            Debug.Log("URL Image: " + ImmgUrl);
         }
         else
         {
-
+            Debug.LogError("Échec de l'authentification : " + status);
         }
     }
-
 }
+
