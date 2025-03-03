07a33b68e2047c2c1024cc8b1c8101ed9e8fa6b7
NicoDuchesne <nicolas.duchesne@edu.devinci.fr>
2025-03-03 15:57:35 +0100

    Preparing new valves

---
+++ Assets/Scripts/ValveSprites.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValveSprites : MonoBehaviour
+{
+    public Sprite[] simpleDisable;
+    public Sprite[] simpleEnable;
+
+    public Sprite[] simpleDisableRot;
+    public Sprite[] simpleEnableRot;
+
+    public Sprite[] doubleDisable;
+    public Sprite[] doubleMiddle;
+    public Sprite[] doubleEnable;
+
+    public Sprite[] doubleDisableRot;
+    public Sprite[] doubleMiddleRot;
+    public Sprite[] doubleEnableRot;
+
+    public Sprite[] simpleDisableCourb;
+    public Sprite[] simpleEnableCourb;
+
+    public Sprite[] simpleDisableRotCourb;
+    public Sprite[] simpleEnableRotCourb;
+
+    public Sprite[] doubleDisableCourb;
+    public Sprite[] doubleMiddleCourb;
+    public Sprite[] doubleEnableCourb;
+
+    public Sprite[] doubleDisableRotCourb;
+    public Sprite[] doubleMiddleRotCourb;
+    public Sprite[] doubleEnableRotCourb;
+}
