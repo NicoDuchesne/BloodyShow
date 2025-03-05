377754d9163c1aa36ea8f5a23e43f8bea32d5d53
Rayan <rayanngi20@gmail.com>
2025-03-05 13:28:20 +0100

    fix tuto 1 /2 /3 /4

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Dialogue : MonoBehaviour
 {
@@ -9,6 +10,22 @@ public class Dialogue : MonoBehaviour
     public string[] lines;
     public float textSpeed;
     private int index;
+
+    //tuto
+    public GameObject dialogue1;
+    public GameObject blockScreen;
+
+    //tuto 2
+    public GameObject dialogue2;
+
+    //tuto 3
+    public GameObject dialogue3;
+
+    //tuto 4
+    public GameObject dialogue4;
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -49,6 +66,31 @@ public class Dialogue : MonoBehaviour
         else
         {
             gameObject.SetActive(false);
+            Scene scene = SceneManager.GetActiveScene();
+
+            if (scene.name == "Niveau1")
+            {
+                dialogue1.SetActive(false);
+                blockScreen.SetActive(false);
+            }
+
+            if (scene.name == "Niveau2")
+            {
+                dialogue2.SetActive(false);
+                blockScreen.SetActive(false);
+            }
+
+            if (scene.name == "Niveau3")
+            {
+                dialogue3.SetActive(false);
+                blockScreen.SetActive(false);
+            }
+
+            if (scene.name == "Niveau4")
+            {
+                dialogue4.SetActive(false);
+                blockScreen.SetActive(false);
+            }
         }
     }
 
