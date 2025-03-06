5285a1254d12a761c831ac2e596ca24e78576bc5
NicoDuchesne <nicolas.duchesne@edu.devinci.fr>
2025-03-06 13:15:25 +0100

    anim scene 2

---
+++ Assets/Scripts/EnableAnim.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EnableAnim : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    public void LaunchAnim() { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
